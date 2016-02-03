0ca7951b5063d305ef52bc353f97f1cafbb620a9
Geir Gåsodden <geir.gasodden@pythonia.no>
2016-02-03 15:03:42 +0100

    Separate YAR_SECRET

--- config/index.js
+++ config/index.js
@@ -27,8 +27,10 @@ var config = {
   DB_CONNECTION_LOG: process.env.DB_CONNECTION_LOG || 'mongodb://logserver/louie',
   DB_CONNECTION_QUEUE: process.env.DB_CONNECTION_QUEUE || 'mongodb://queueserver/louie',
   BUDDY_API_URL: process.env.BUDDY_API_URL || 'https://api.buddy.com',
+  CALLBACK_STATUS_URL: process.env.CALLBACK_STATUS_URL || 'https://api.buddy.com/status/',
   JWT_SECRET: process.env.JWT_SECRET || 'Louie Louie, oh no, I got to go',
-  COOKIE_SECRET: process.env.JWT_SECRET || 'Louie Louie, oh no, I got to go',
+  COOKIE_SECRET: process.env.COOKIE_SECRET || 'Louie Louie, oh no, I got to go',
+  YAR_SECRET: process.env.YAR_SECRET || 'Louie Louie, oh no, I got to go',
   LDAP: {
     url: process.env.LDAP_URL || 'ldap://ldap.forumsys.com:389',
     bindDn: process.env.LDAP_BIND_DN || 'cn=read-only-admin,dc=example,dc=com',
