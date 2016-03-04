d6a2efb2779d62a1db580918fca6eaf42e9ad71e
Geir Gåsodden <geir.gasodden@pythonia.no>
2016-03-04 15:20:07 +0100

    Implements feedback. Fixes #22

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 'use strict'
 
 var routes = require('./routes')
+var feedback = require('./routes/feedback')
 
 exports.register = function (server, options, next) {
   server.route(routes)
+  server.route(feedback)
   next()
 }
 
