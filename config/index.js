387c9431ae5bbb96ced13634ee109fdf45f1c656
Geir Gåsodden <geir.gasodden@pythonia.no>
2016-05-20 16:45:18 +0200

    Implements namespaced envs

--- config/index.js
+++ config/index.js
@@ -7,17 +7,17 @@
 
 var path = require('path')
 
-const SERVER_PORT_WEB = process.env.SERVER_PORT_WEB || 8000
+const SERVER_PORT_WEB = process.env.MINELEV_SERVER_PORT_WEB || 8000
 
 function ldapTlsSettings () {
   var fs = require('fs')
   var config = false
 
-  if (process.env.LDAP_TLS_SETTINGS) {
+  if (process.env.MINELEV_LDAP_TLS_SETTINGS) {
     config = {
-      rejectUnauthorized: process.env.LDAP_TLS_REJECT_UNAUTHORIZED ? true : false, // eslint-disable-line no-unneeded-ternary
+      rejectUnauthorized: process.env.MINELEV_LDAP_TLS_REJECT_UNAUTHORIZED ? true : false, // eslint-disable-line no-unneeded-ternary
       ca: [
-        fs.readFileSync(path.join(__dirname, process.env.LDAP_TLS_CA_PATH))
+        fs.readFileSync(path.join(__dirname, process.env.MINELEV_LDAP_TLS_CA_PATH))
       ]
     }
   }
@@ -27,22 +27,22 @@ function ldapTlsSettings () {
 
 var config = {
   SERVER_PORT_WEB: SERVER_PORT_WEB,
-  DB_CONNECTION_LOG: process.env.DB_CONNECTION_LOG || 'mongodb://localhost/louie',
-  DB_CONNECTION_QUEUE: process.env.DB_CONNECTION_QUEUE || 'mongodb://localhost/louie',
-  BUDDY_API_URL: process.env.BUDDY_API_URL || 'http://localhost:' + SERVER_PORT_WEB + '/demo',
-  CALLBACK_STATUS_URL: process.env.CALLBACK_STATUS_URL || 'https://api.buddy.com/status/',
-  TEMPLATER_SERVICE_URL: process.env.TEMPLATER_SERVICE_URL || 'https://api.buddy.com/status/',
-  JWT_SECRET: process.env.JWT_SECRET || 'Louie Louie, oh no, I got to go',
-  COOKIE_SECRET: process.env.COOKIE_SECRET || 'Louie Louie, oh no, I got to go. Louie Louie, oh no, I got to go',
-  GITHUB_FEEDBACK_URL: process.env.GITHUB_FEEDBACK_URL || 'https://api.github.com/repos/:user/:repo/issues',
-  GITHUB_USER: process.env.GITHUB_USER || 'yourgithubuser',
-  GITHUB_TOKEN: process.env.GITHUB_TOKEN || 'yourgithubtoken',
+  DB_CONNECTION_LOG: process.env.MINELEV_LOG_DB_URI || 'mongodb://localhost/louie',
+  DB_CONNECTION_QUEUE: process.env.MINELEV_QUEUE_DB_URI || 'mongodb://localhost/louie',
+  BUDDY_API_URL: process.env.MINELEV_BUDDY_API_URL || 'http://localhost:' + SERVER_PORT_WEB + '/demo',
+  CALLBACK_STATUS_URL: process.env.MINELEV_CALLBACK_STATUS_URL || 'https://api.buddy.com/status/',
+  TEMPLATER_SERVICE_URL: process.env.MINELEV_TEMPLATER_SERVICE_URL || 'https://api.buddy.com/status/',
+  JWT_SECRET: process.env.MINELEV_JWT_SECRET || 'Louie Louie, oh no, I got to go',
+  COOKIE_SECRET: process.env.MINELEV_COOKIE_SECRET || 'Louie Louie, oh no, I got to go. Louie Louie, oh no, I got to go',
+  GITHUB_FEEDBACK_URL: process.env.MINELEV_GITHUB_FEEDBACK_URL || 'https://api.github.com/repos/:user/:repo/issues',
+  GITHUB_USER: process.env.MINELEV_GITHUB_USER || 'yourgithubuser',
+  GITHUB_TOKEN: process.env.MINELEV_GITHUB_TOKEN || 'yourgithubtoken',
   LDAP: {
-    url: process.env.LDAP_URL || 'ldap://ldap.forumsys.com:389',
-    bindDn: process.env.LDAP_BIND_DN || 'cn=read-only-admin,dc=example,dc=com',
-    bindCredentials: process.env.LDAP_BIND_CREDENTIALS || 'password',
-    searchBase: process.env.LDAP_SEARCH_BASE || 'dc=example,dc=com',
-    searchFilter: process.env.LDAP_SEARCH_FILTER || '(uid={{username}})',
+    url: process.env.MINELEV_LDAP_URL || 'ldap://ldap.forumsys.com:389',
+    bindDn: process.env.MINELEV_LDAP_BIND_DN || 'cn=read-only-admin,dc=example,dc=com',
+    bindCredentials: process.env.MINELEV_LDAP_BIND_CREDENTIALS || 'password',
+    searchBase: process.env.MINELEV_LDAP_SEARCH_BASE || 'dc=example,dc=com',
+    searchFilter: process.env.MINELEV_LDAP_SEARCH_FILTER || '(uid={{username}})',
     tlsOptions: ldapTlsSettings()
   }
 }
