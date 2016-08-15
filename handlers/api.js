0084d3e36b02792df8cab63253daa9f4f28ff24a
Geir Gåsodden <geir.gasodden@pythonia.no>
2016-08-15 07:57:47 +0200

    Adds seneca info message on statuslog updates

--- handlers/api.js
+++ handlers/api.js
@@ -1,17 +1,18 @@
 'use strict'
 
-var mongojs = require('mongojs')
-var config = require('../config')
-var dblog = mongojs(config.DB_CONNECTION_LOG)
-var logs = dblog.collection('logs')
+const mongojs = require('mongojs')
+const config = require('../config')
+const dblog = mongojs(config.DB_CONNECTION_LOG)
+const logs = dblog.collection('logs')
 
-function addStatusToLog (request, reply) {
-  var documentId = request.params.documentId
+module.exports.addStatusToLog = (request, reply) => {
+  const documentId = request.params.documentId
   var status = request.payload
   status.timeStamp = new Date().getTime()
+
+  request.seneca.act({role: 'info', info: 'statuslog', msg: 'add', data: status})
+
   logs.update({'documentId': documentId}, {'$push': {documentStatus: status}}, function (error, data) {
     reply(error || data)
   })
 }
-
-module.exports.addStatusToLog = addStatusToLog
